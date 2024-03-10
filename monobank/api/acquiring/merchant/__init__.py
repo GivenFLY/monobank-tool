4a63ae77060bc702da9f0dddfb213e34b814676b
GivenFLY <alexey.givenfly.moshura@gmail.com>
2024-03-10 22:32:54 +0200

    added nested facades to MerchantFacade

--- monobank/api/acquiring/merchant/__init__.py
+++ monobank/api/acquiring/merchant/__init__.py
@@ -1,4 +1,9 @@
+from monobank.api.acquiring.merchant.details import DetailsFacade
 from monobank.api.acquiring.merchant.invoice import InvoiceFacade
+from monobank.api.acquiring.merchant.pubkey import PubkeyFacade
+from monobank.api.acquiring.merchant.qr import QRFacade
+from monobank.api.acquiring.merchant.statement import StatementFacade
+from monobank.api.acquiring.merchant.wallet import WalletFacade
 from monobank.core.client.acquiring import AcquiringAPIClient
 
 
@@ -6,3 +11,8 @@ class MerchantFacade:
     def __init__(self, client: AcquiringAPIClient):
         self.client = client
         self.invoice = InvoiceFacade(client=client)
+        self.details = DetailsFacade(client=client)
+        self.pubkey = PubkeyFacade(client=client)
+        self.qr = QRFacade(client=client)
+        self.statement = StatementFacade(client=client)
+        self.wallet = WalletFacade(client=client)
